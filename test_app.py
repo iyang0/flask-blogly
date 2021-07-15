05a2682ea396130120400b25975734a491e7b2c3
magus0 <harvestashya@gmail.com>
2021-07-15 13:52:09 -0700

    more unit test fixing

--- test_app.py
+++ test_app.py
@@ -5,12 +5,29 @@ from models import db, User
 # Make Flask errors be real errors, not HTML pages with error info
 app.config['TESTING'] = True
 
-class BoggleAppTestCase(TestCase):
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
+app.config['SQLALCHEMY_ECHO'] = False
+
+db.drop_all()
+db.create_all()
+
+class BloglyAppTestCase(TestCase):
     
     def setUp(self):
         """Stuff to do before every test."""
+        User.query.delete()
+        
         self.client = app.test_client()
         app.config['TESTING'] = True
+        zach = User(first_name="Zach", last_name="Thomas")
+        ivan = User(first_name="Ivan", last_name="Yang")
+        db.session.add_all([zach, ivan])
+        db.session.commit()
+        
+        
+    def tearDown(self):
+        """Clean up any fouled transaction."""
+        db.session.rollback()
         
     def test_root(self):
         """Make sure that the root redirects correctly """
@@ -51,7 +68,7 @@ class BoggleAppTestCase(TestCase):
         """Send a post request hitting the /users/<user_id>/edit form"""
         
         with self.client as client:
-            test_user = User.query.filter(User.first_name=="API").one()
+            test_user = User.query.filter(User.first_name=="Ivan").first()
             
             response = client.post(f'/users/{test_user.id}/edit',
                 data = {
