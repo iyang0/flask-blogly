c37e6bde5cea359ded15c3b1ecaaddaf9c4a5506
ZeeTom <zthom13@hotmail.com>
2021-07-15 19:08:14 -0600

    many to many relationship

--- models.py
+++ models.py
@@ -1,7 +1,10 @@
 """Models for Blogly."""
 
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+from sqlalchemy.orm import backref
 
 db = SQLAlchemy()
 DEFAULT_IMAGE = 'https://i.imgur.com/2NFyNvK.jpeg'
@@ -34,6 +37,33 @@ class User(db.Model):
         backref='author')
 
 
+class PostTag(db.Model):
+    """Creates join table between tags and posts"""
+
+    __tablename__ = 'posts_tags'
+
+    post_id = db.Column(db.Integer,
+        db.ForeignKey('posts.id'),
+        primary_key=True)
+
+    tag_id = db.Column(db.Integer,
+        db.ForeignKey('tags.id'),
+        primary_key=True)
+        
+
+class Tag(db.Model):
+    """Creates tag class/table"""
+    
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    name = db.Column(db.Text,
+        nullable=False,
+        unique=True)
+
 class Post(db.Model):
     """Creates class for user posts.
     Table creation/schema in blogly database."""
@@ -57,5 +87,13 @@ class Post(db.Model):
     user_id = db.Column(db.Integer,
         db.ForeignKey('users.id'),
         nullable=False)
-        
+
+    tags = db.relationship('Tag',
+        secondary='posts_tags',
+        backref='posts'
+    )
+
+
+
+
 
